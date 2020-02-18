35d051c7a4d005a9bb47d5675ff7559117708943
Saul <saulkrujoski@gmail.com>
2020-02-18 17:24:57 -0300

    Cambios en Event page, mejora visual y responsiva de slides de imágenes

--- src/app/event/event.page.spec.ts
+++ src/app/event/event.page.spec.ts
@@ -1,24 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
-import { EventPage } from './event.page';
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.page.html',
+  styleUrls: ['./event.page.scss'],
+})
+export class EventPage implements OnInit {
 
-describe('EventPage', () => {
-  let component: EventPage;
-  let fixture: ComponentFixture<EventPage>;
+  @ViewChild('slideWithNav', {static: false}) slideWithNav: IonSlides;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ EventPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  sliderOne: any;
+  
+  // este será el manejador del slider image
+  slideOptsOne = {initialSlide: 0,slidesPerView: 1,autoplay:false};
 
-    fixture = TestBed.createComponent(EventPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+  // uso esta variable apra poder simular la carga de un conjunto de imágenes
+  fotos: any[] = Array(8);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  constructor() {this.sliderOne ={isBeginningSlide: true,isEndSlide: false};}
+
+  //Method called when slide is changed by drag or navigation
+  SlideDidChange(object, slideView) {this.checkIfNavDisabled(object, slideView);}
+ 
+  //Call methods to check if slide is first or last to enable disbale navigation  
+  checkIfNavDisabled(object, slideView) {this.checkisBeginning(object, slideView);this.checkisEnd(object, slideView);}
+ 
+  checkisBeginning(object, slideView) {slideView.isBeginning().then((istrue) => {object.isBeginningSlide = istrue;});}
+
+  checkisEnd(object, slideView) {slideView.isEnd().then((istrue) => {object.isEndSlide = istrue;});}
+
+  ngOnInit() {}
+
+}
